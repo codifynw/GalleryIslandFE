330d57dc5081e6d0bbd431f0fa606f7021b93cc8
Cody Wilson <cody@codyscapes.com>
2019-07-08 19:24:12 -0700

    index working with webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+// const CircularDependencyPlugin = require('circular-dependency-plugin')
+// const core_path = path.resolve(__dirname, './../../../source_framework/static/js/modules/');
+// const module_path = path.resolve(__dirname, './../../../source_framework/static/js/source/');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    // mode: 'development',
+    entry: {
+        main: [
+            './src/App.js'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    }
+    // devtool: 'inline-source-map'
+};
