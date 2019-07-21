951a8d8024ed3e1d50fa8adcd49f207e7845eb9c
Cody Wilson <cody@codyscapes.com>
2019-07-20 20:32:30 -0700

    super simple dev server and html loader config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 // const CircularDependencyPlugin = require('circular-dependency-plugin')
 // const core_path = path.resolve(__dirname, './../../../source_framework/static/js/modules/');
 // const module_path = path.resolve(__dirname, './../../../source_framework/static/js/source/');
@@ -12,21 +14,32 @@ module.exports = {
             './src/App.js'
         ]
     },
-      module: {
+    module: {
         rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-          }
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
         ]
-      },
-      resolve: {
-        extensions: ['*', '.js', '.jsx']
-      },
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: false,
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/dist',
+        compress: true,
+        port: 9000,
+        stats: 'errors-only'
     }
     // devtool: 'inline-source-map'
 };
